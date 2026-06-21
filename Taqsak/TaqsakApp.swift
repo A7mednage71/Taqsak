ab93a04ee125857ce562a8c1d5b65092b138654e
Ahmed Nageh <ahmdnajkh71@gmail.com>
2026-06-21 17:25:16 +0300

    Initial Commit

---
+++ Taqsak/TaqsakApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaqsakApp.swift
+//  Taqsak
+//
+//  Created by Ahmed Nageh on 21/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct TaqsakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
